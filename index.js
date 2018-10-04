b06876e29a26cb9bfb32f11c27827a3386526520
Jason Tremper <jason@mechavian.com>
2018-10-03 20:01:08 -0400

    adding error logging

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
 const Gpio = require('onoff').Gpio;
 const webcam = require('./webcam');
 
-(async function() {
-    const led = new Gpio(17, 'out');
-    let count = 10;
+try {
+    (async function() {
+        const led = new Gpio(17, 'out');
+        let count = 10;
 
-    setLed(led, true);
-    await webcam.startCaptureAsync();
-    console.log('Starting webcam...');
+        setLed(led, true);
+        await webcam.startCaptureAsync();
+        console.log('Starting webcam...');
 
-    const timer = setInterval(async () => {
-        if (count-- > 0) {
-            console.log('.');
-        } else {
-            console.log('...stopping webcam');
-            await webcam.stopCaptureAsync();
-            setLed(led, false);
+        // TODO: Need a more precise timer
+        const timer = setInterval(async () => {
+            if (count-- > 0) {
+                console.log('.');
+            } else {
+                console.log('...stopping webcam');
+                await webcam.stopCaptureAsync();
+                setLed(led, false);
 
-            clearInterval(timer);
+                clearInterval(timer);
+            }
+        }, 1000);
+
+        function setLed(led, value) {
+            if (value) {
+                console.log('LED ON');
+                led.writeSync(1);
+            } else {
+                console.log('LED OFF');
+                led.writeSync(0);
+            }
         }
-    }, 1000);
-
-    function setLed(led, value) {
-        if (value) {
-            console.log('LED ON');
-            led.writeSync(1);
-        } else {
-            console.log('LED OFF');
-            led.writeSync(0);
-        }
-    }
-})();
+    })();
+} catch (error) {
+    console.log(`FATAL ERROR: ${error}`);
+}
